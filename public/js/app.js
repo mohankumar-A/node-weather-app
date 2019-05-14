84fce62a1634874a72768e9e7dd8bca7407a1695
Mohan Kumar <mohan.swtech@gmail.com>
2019-05-14 09:16:15 +0530

    remove unnessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-// console.log("from client side javascript");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input[name=address]")
 const messageOne = document.querySelector("#message-1");
